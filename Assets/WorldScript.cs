97f0f789a00bd58c26ecb5af1546280de06163c8
Kirill Shevchenko <kshevchenko@microsoft.com>
2022-09-19 19:56:51 +0200

    Add a maze center to bind all other objects to it

--- Assets/WorldScript.cs
+++ Assets/WorldScript.cs
@@ -9,6 +9,13 @@ public class WorldScript : MonoBehaviour
     void Start()
     {
         var maze = GetMaze();
+        var middleCoordinates = new Tuple<float, float, float>((float)maze.GetLength(0) / 2, (float)maze.GetLength(1) / 2, (float)maze.GetLength(2) / 2);
+        var cubePrefab = Resources.Load("Prefabs/block_brick_brown_1"); // Assets/Resources/Prefabs/prefab1.FBX
+        var mazeObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        mazeObject.transform.position = new Vector3(middleCoordinates.Item1, middleCoordinates.Item2, middleCoordinates.Item3);
+        mazeObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        mazeObject.name = "maze";
+
         for (int i = 0; i < maze.GetLength(0); i++)
         {
             for (int j = 0; j < maze.GetLength(1); j++)
@@ -17,12 +24,13 @@ public class WorldScript : MonoBehaviour
                 {
                     if (maze[i, j, h])
                     {
-                        UnityEngine.Object prefab = Resources.Load("Prefabs/block_brick_brown_1"); // Assets/Resources/Prefabs/prefab1.FBX
-                        GameObject t = (GameObject)Instantiate(prefab, new Vector3(i, j, h), Quaternion.identity);
+                        var cubeObject = (GameObject)Instantiate(cubePrefab, new Vector3(i, j, h), Quaternion.identity);
+                        cubeObject.transform.parent = mazeObject.transform;
                     }
                 }
             }
         }
+
     }
 
     // Update is called once per frame
