9bb263ad86577d8588a2ee074a5f29888aa48536
Kirill Shevchenko <kirillwowlol@yandex.ru>
2022-09-20 14:59:02 +0200

    Add rotation of the maze

--- Assets/UserControl.cs
+++ Assets/UserControl.cs
@@ -6,28 +6,57 @@ using UnityEngine.XR;
 public class UserControl : MonoBehaviour
 {
     InputDevice rdevice;
+    InputDevice head;
     Vector2 primary2DAxis;
+    Quaternion headRotation;
+    GameObject maze;
+    GameObject user;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        maze = GameObject.Find("maze001");
+        user = GameObject.Find("XR Origin");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rdevice.isValid)
+
+        //maze.transform.RotateAround(user.transform.position, new Vector3(
+        //    Mathf.Cos(user.transform.rotation.eulerAngles.y * Mathf.Deg2Rad), 
+        //    0,
+        //    -Mathf.Sin(user.transform.rotation.eulerAngles.y * Mathf.Deg2Rad)), 
+        //    0.1f);
+
+        if (rdevice.isValid && head.isValid)
         {
-            
             rdevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out primary2DAxis);
-            Debug.Log(string.Format("x:{0}, y:{1}", primary2DAxis.x, primary2DAxis.y));
+            head.TryGetFeatureValue(CommonUsages.deviceRotation, out headRotation);
+            Debug.Log($" y:{headRotation.eulerAngles.y}");
+            if (primary2DAxis.y != 0)
+            {
+                maze.transform.RotateAround(user.transform.position, new Vector3(
+                    Mathf.Cos(headRotation.eulerAngles.y * Mathf.Deg2Rad),
+                    0,
+                    -Mathf.Sin(headRotation.eulerAngles.y * Mathf.Deg2Rad)),
+                    primary2DAxis.y);
+            }
         }
         else
         {
-            var rightHandDevices = new List<UnityEngine.XR.InputDevice>();
-            InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
-            rdevice = rightHandDevices.FirstOrDefault();
+            if (!rdevice.isValid)
+            {
+                var rightHandDevices = new List<UnityEngine.XR.InputDevice>();
+                InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
+                rdevice = rightHandDevices.FirstOrDefault();
+            }
+            if (!head.isValid)
+            {
+                var headDevices = new List<UnityEngine.XR.InputDevice>();
+                InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.Head, headDevices);
+                head = headDevices.FirstOrDefault();
+            }
         }
     }
 }
