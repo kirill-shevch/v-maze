18587e2fb674db9d0cfc43b45a8f9771b87418cc
Kirill Shevchenko <kirillwowlol@yandex.ru>
2022-09-22 11:26:35 +0200

    Fix XR components and rever unnecessary changes

--- Assets/Scripts/WorldScript.cs
+++ Assets/Scripts/WorldScript.cs
@@ -85,14 +85,6 @@ public class WorldScript : MonoBehaviour
         maze[1, 2, 2] = false;
         maze[1, 2, 3] = false;
         maze[1, 2, 4] = false;
-
-        // Optionally close the entrance, exist to simulate lightning in closed env.
-        // if (!isClosed)
-        // {
-        //     maze[1, 1, 0] = false;
-        //     maze[1, 2, 4] = false;
-        // }
-
         return maze;
     }
 
